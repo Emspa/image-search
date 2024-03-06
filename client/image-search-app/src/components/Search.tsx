b9f32b541cb99c211ab6f869ebae84599789cda9
Emili Spasovska <emilis95@hotmail.com>
2024-03-06 11:55:29 +0100

    Added correctedQuery functionality and styled it

--- client/image-search-app/src/components/Search.tsx
+++ client/image-search-app/src/components/Search.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { IGoogleSearchResponse } from "../models/IGoogleSearchResponse";
 import { SearchResult } from "./SearchResults";
 import './SearchResults.css'
@@ -15,28 +15,27 @@ export const Search = () => {
     setSearchQuery(e.target.value);
   }
 
-  const handleSearch = async () => {
+  const handleSearch = async (query?: string) => {
+    const searchTerm = query || searchQuery;
     const response = await fetch(
       `https://www.googleapis.com/customsearch/v1?key=${
         import.meta.env.VITE_GOOGLE_API_KEY
-      }&cx=${import.meta.env.VITE_GOOGLE_ID}&num=10&searchType=image&q=${searchQuery}`
+      }&cx=${import.meta.env.VITE_GOOGLE_ID}&num=10&searchType=image&q=${searchTerm}`
     );
     const data: IGoogleSearchResponse = await response.json();
     console.log(data);
     setSearchData(data);
     setSearchQuery("");
+
   };
 
   return (
     <div className="search-container">
         <div className="search-input-container">
         <input type="text" value={searchQuery} onChange={handleSearchInput} />
-        <button onClick={handleSearch}>Search</button>
+        <button onClick={() => handleSearch()}>Search</button>
       </div>
-      <div className="search-time">
-          Search time: {searchData?.searchInformation.formattedSearchTime} sec
-        </div>            
-      <SearchResult searchData={searchData}/>
+      <SearchResult searchData={searchData} handleSearch={handleSearch}/>
 
     </div>
   );
--- client/image-search-app/src/components/SearchResults.tsx
+++ client/image-search-app/src/components/SearchResults.tsx
@@ -1,28 +1,46 @@
-import { IGoogleSearchResponse } from "../models/IGoogleSearchResponse"
-import './SearchResults.css'
+/** @format */
+
+import { IGoogleSearchResponse } from "../models/IGoogleSearchResponse";
+import "./SearchResults.css";
 
 interface ISearchResultProps {
-    searchData: IGoogleSearchResponse | null;
+  searchData: IGoogleSearchResponse | null;
+  handleSearch: (query?: string) => Promise<void>;
 }
 
-export const SearchResult = ({searchData}: ISearchResultProps ) => {  if (!searchData || !searchData.items) {
-    return <p>No images found.</p>; 
-  }
+export const SearchResult = ({
+  searchData,
+  handleSearch,
+}: ISearchResultProps) => {
+
     return (
-        <div className="div-container">
-            <div>
-            </div>
-            <div className="images-container">
-            {searchData.items.map((item, i) => (
-                <div key={i}>
-                    <img className="images" src={item.link} alt={item.title} />
-                </div>
-
-            ))}
-
-            </div>
-
-
+    <div className="div-container">
+      <div className="search-info">
+      {searchData?.items && (
+        <div className="search-time">
+          Time: {searchData?.searchInformation.formattedSearchTime}
         </div>
-    )
-}
+      )}
+      {searchData?.spelling?.correctedQuery && (
+        <div className="corrected-query">
+          <p>Did you mean:</p>
+          <a
+            onClick={() => handleSearch(searchData?.spelling?.correctedQuery)}
+          >
+            {searchData.spelling.correctedQuery}?
+          </a>
+        </div>
+      )}
+
+      </div>
+  
+      <div className="images-container">
+        {searchData?.items.map((item, i) => (
+          <div key={i}>
+            <img className="images" src={item.link} alt={item.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
