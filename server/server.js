b4fe45d6580b142e88641854b1b230fe43dc538b
Emili Spasovska <emilis95@hotmail.com>
2024-03-07 16:32:40 +0100

    Created a new route in the Express server to handle POST requests to save favorite images.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const fs = require('fs')
 
 const cors = require("cors")
 
@@ -18,9 +19,20 @@ const images = [
 app.use(cors());
 app.use(express.json())
 
-app.get('/images', (req, res) => {
-    res.status(200).json(images)
+// app.get('/images', (req, res) => {
+//     res.status(200).json(images)
 
-})
+// })
+
+
+app.post('/api/save-favorite', async (req, res) => {
+    console.log(req.body);
+    // Validera inkommande data med Joi här
+    const { user, imageUrl } = req.body; // Antag att dessa fält finns i din request body
+
+    // Här skulle du spara datan i din JSON-fil
+    
+    res.json({ message: 'Bild sparad' });
+  });
 
 app.listen(3000, () => console.log("Server is up and running..."))
