37770a21b5e339606d4cf2974e09f51770aa9cb1
Emili Spasovska <emilis95@hotmail.com>
2024-03-09 18:40:41 +0100

    Fetch and display user favorite

--- client/image-search-app/src/components/FavoriteImages.tsx
+++ client/image-search-app/src/components/FavoriteImages.tsx
@@ -1,19 +1,49 @@
-import { useState } from "react";
-import { IImages } from "../models/IImages"
+import { useEffect, useState } from "react";
+
+import { useAuth0 } from "@auth0/auth0-react";
+import { IImageResultItem } from "../models/IImageResultItem";
+import axios from "axios";
 
 
 export const FavoriteImages = () => {
+    const {user} = useAuth0()
 
-    const [favorite, setFavorite] = useState<IImages[]>([]);
+    const [favorites, setFavorites] = useState<IImageResultItem[]>([]);
+
+    const userId = user?.sub;
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/api/users/favorite/${userId}`);
+                setFavorites(response.data)
+                console.log(response.data)
+
+            }catch (error) {
+                console.log("Error fetching data", error)
+            }
+        };
+        fetchData()
+    },[]);
 
 
     
-    return(
+      return (
         <div>
- 
-         
-
-
+            <h2>My Photos</h2>
+            {favorites.length > 0 ? (
+                <ul>
+                    {favorites?.map((item, i) => (
+                        <li key={i}>
+                            <img src={item.link} alt={item.title}  />
+                            
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No pictures available</p>
+            )}
         </div>
-    )
+    );
+    
 }
--- client/image-search-app/src/pages/MyFavorites.tsx
+++ client/image-search-app/src/pages/MyFavorites.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { IImages } from "../models/IImages";
+
 import { FavoriteImages } from "../components/FavoriteImages";
 
 export const MyFavorites = () => {
